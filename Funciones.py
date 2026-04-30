2b10f812913a50f048217a52c3f77abc5c113fe3
yonder maldonado <yondermaldonado23@gmail.com>
2026-04-30 15:48:07 -0500

    feat: regirtar paquete

--- Funciones.py
+++ Funciones.py
@@ -5,6 +5,18 @@ def leerArchivo(ruta):
             return json.load(file)
     except Exception:
         return []
+    
 def guardarArchivo(ruta,datos):
     with open(ruta,"w")as file:
         json.dump(datos,file,indent=4)
+
+def registrarPaquete(listaPaquetes):
+    diccionarioPaquetes = {
+        "Nombre":input("digite nombre: "),
+        "Cantidad":input("digite el numero de fotos: "),
+        "Precio":input("digite precio: "),
+        "Duracion":input("digite la duracion en horas: ")
+    }
+    listaPaquetes.append(diccionarioPaquetes)
+    guardarArchivo("Paquetes.json",listaPaquetes)
+    return listaPaquetes
