84ec9d442fd1fda8d63f7bf1c0338530892f7c66
Jesus Salcedo <Tecnicajesus20@gmail.com>
2026-04-30 16:06:16 -0500

    Fix : FUNCION VERPRODUCTOS ARREGLADA

--- Funciones.py
+++ Funciones.py
@@ -1,3 +1,15 @@
 import json
-def VerProductos():
-    leerArchivos(Paquetes.json)   
+def leerArchivo(ruta):
+    try:
+        with open(ruta,"r")as file:
+            return json.load(file)
+    except Exception:
+        return []
+    
+def guardarArchivo(ruta,datos):
+    with open(ruta,"w")as file:
+        json.dump(datos,file,indent=4)
+def VerPaquetes():
+    Contenido = leerArchivo("Paquetes.json") 
+    print (Contenido)
+    
